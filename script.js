a0afd09e0d77075e52bdc5b9916d3f6928fba62d
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-04-03 13:18:16 -0600

    1st practice done

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const adventurer = {
+  name: "Robin",
+  health: 10,
+  inventory: ["sword", "potion", "artifact"],
+  companion: {
+    name: "Leo",
+    type: "Cat",
+  },
+};
 
+//// 1st practice
+for (let items of adventurer.inventory) {
+  console.log(items);
+}
