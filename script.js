9813019e9c4ccf1513df64fd9a496c11f1a0a4cd
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-04-03 13:42:24 -0600

    part two done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// part one
 const adventurer = {
   name: "Robin",
   health: 10,
@@ -14,10 +15,10 @@ const adventurer = {
 
 //// 1st practice
 for (let items of adventurer.inventory) {
-  console.log(items);
+  //   console.log(items);
 }
 
-// 2
+// 2 practice
 let subObj = {
   name: "Frank",
   type: "Flea",
@@ -25,6 +26,32 @@ let subObj = {
 };
 adventurer.companion.leo = subObj;
 
-console.log(adventurer.roll());
-console.log(adventurer.roll());
-console.log(adventurer.roll());
+// console.log(adventurer.roll());
+// console.log(adventurer.roll());
+// console.log(adventurer.roll());
+
+////
+///////////////
+//// part two
+
+class Character {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.inventory = [];
+  }
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+  }
+}
+
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+// console.log(robin.companion.companion.roll());
